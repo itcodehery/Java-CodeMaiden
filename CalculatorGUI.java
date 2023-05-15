1fc7dabf73c7eb66324d4132628c6b6556db9a6b
Hari <haririo321@gmail.com>
2023-05-15 19:44:06 +0530

    Added the operator and clear buttons

--- CalculatorGUI.java
+++ CalculatorGUI.java
@@ -9,6 +9,36 @@ import java.awt.event.*;
 
 public class CalculatorGUI extends JFrame implements ActionListener{
 	JTextField displayField = new JTextField();
+	
+	//This method creates a Jbutton with the specified font, color and rounded corners
+	private JButton createButton(String text) {
+        JButton button = new JButton(text);
+        button.addActionListener(this);
+        button.setFont(new Font("Arial", Font.BOLD, 20));
+        button.setBackground(Color.WHITE);
+        button.setFocusable(false);
+
+        Border roundedBorder = new RoundedBorder(15, Color.BLACK);
+        Border compoundBorder = new CompoundBorder(roundedBorder, new EmptyBorder(10, 10, 10, 10));
+        button.setBorder(compoundBorder);
+
+        return button;
+    }
+	
+	private JButton createClearButton(){
+        JButton button = new JButton("C");
+        button.addActionListener(this);
+        button.setFont(new Font("Arial", Font.BOLD, 20));
+        button.setBackground(Color.WHITE);
+        button.setFocusable(false);
+
+        Border roundedBorder = new RoundedBorder(15, Color.BLACK);
+        Border compoundBorder = new CompoundBorder(roundedBorder, new EmptyBorder(10, 10, 10, 10));
+        button.setBorder(compoundBorder);
+
+        return button;
+    }
+	
 	public CalculatorGUI() {
 		//Set frame properties
 		setTitle("Calculator");
@@ -53,8 +83,56 @@ public class CalculatorGUI extends JFrame implements ActionListener{
 		zeroButton.setBorder(compoundBorder);
 		numberPanel.add(zeroButton);
 		
-		//adding number panel to the frame
+		//creating the operator panel
+		JPanel operatorPanel = new JPanel(new GridLayout(4, 1));
+        JButton addButton =  createButton("+");
+        JButton subtractButton = createButton("-");
+        JButton multiplyButton = createButton("*");
+        JButton divideButton = createButton("/");
+
+        operatorPanel.add(addButton);
+        operatorPanel.add(subtractButton);
+        operatorPanel.add(multiplyButton);
+        operatorPanel.add(divideButton);
+		
+        //adding both panels to the frame
 		add(numberPanel, BorderLayout.CENTER);
+		add(operatorPanel, BorderLayout.CENTER);
+		
+		// creating the clear panel
+		JPanel clearPanel = new JPanel(new GridLayout(1,1));
+		JButton clearButton = createClearButton();
+		clearPanel.add(clearButton);
+		add(clearPanel, BorderLayout.CENTER);
+		
+		//adding a super panel: buttonPanel for all sub panels
+		JPanel buttonPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.BOTH;
+        buttonPanel.add(numberPanel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.weightx = 0.3;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.BOTH;
+        buttonPanel.add(operatorPanel, gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        gbc.weightx = 0.1;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.BOTH;
+        buttonPanel.add(clearPanel, gbc);
+
+        add(buttonPanel, BorderLayout.CENTER);
+
+
 		
 		//set frame size and visibility
 		setSize(300, 400);
@@ -63,16 +141,26 @@ public class CalculatorGUI extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		String command = e.getActionCommand();
-		if(command.equals("0") || command.equals("1") || command.equals("2") || command.equals("3")
-			|| command.equals("4") || command.equals("5") || command.equals("6") || command.equals("7")
-			|| command.equals("8") || command.equals("9")) {
-			displayField.setText(displayField.getText() + command);
-		}
-		else
-		{
-			System.out.println("Do Nothing Basically");
-		}
+	    String command = e.getActionCommand();
+
+	    if (command.matches("[0-9]")) {
+	        displayField.setText(displayField.getText() + command);
+	    } else if (command.equals("+") || command.equals("-") || command.equals("*") || command.equals("/")) {
+	        String currentText = displayField.getText();
+	        String lastChar = currentText.substring(currentText.length() - 1);	    
+	        
+	        // Check if the last character is an operator, replace it with the new operator
+	        // 
+	        if (lastChar.matches("[+\\-*/]")) {
+	            currentText = currentText.substring(0, currentText.length() - 1) + command;
+	        } else {
+	            currentText += command;
+	        }
+
+	        displayField.setText(currentText);
+	    } else if (command.equals("C")) {
+	        displayField.setText("");
+	    }
 	}
 	
 	public static void main(String[] args) {
